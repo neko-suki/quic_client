adea82d42061eeca97fc234bd276712352208068
neko-suki <s1150092@gmail.com>
2022-12-21 22:56:32 +0900

    refactor

--- quic/tls/supported_groups.cpp
+++ quic/tls/supported_groups.cpp
@@ -3,9 +3,6 @@
 namespace tls {
 
 NamedGroupList::NamedGroupList() {
-  // secp256r1(0x0017),
-  named_group_list.push_back(0x00);
-  named_group_list.push_back(0x17);
 }
 
 std::vector<uint8_t> NamedGroupList::GetBinary() {
@@ -16,9 +13,15 @@ std::vector<uint8_t> NamedGroupList::GetBinary() {
   // length
   ret.push_back(0x00);
   ret.push_back(0x04);
+
+  // secp256r1(0x0017),
+  named_group_list.push_back(0x00);
+  named_group_list.push_back(0x17);
+
   // size of named curve list: 0x002
   ret.push_back(0x00);
-  ret.push_back(0x02);
+  ret.push_back(named_group_list.size());
+
   std::copy(named_group_list.begin(), named_group_list.end(),
             std::back_inserter(ret));
   return ret;
